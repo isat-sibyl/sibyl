5ce4fb2742ef7f4c9c778db7b5a6932aecc811f1
Pedro Cavalheiro <pedro.cavalheiro@mail.com>
2022-08-03 17:26:38 +0100

    Fixed variable error in copy script

--- init.py
+++ init.py
@@ -3,4 +3,4 @@ import shutil
 if __name__ == '__main__':
 	copy = ['components', 'layouts', 'locales', 'pages', 'static', 'settings.py', 'requirements.txt', 'dev.bat', 'build.bat']
 	for to_copy in copy:
-		shutil.copytree("/sibyl/" + to_copy, "/" + copy)
+		shutil.copytree("/sibyl/" + to_copy, "/" + to_copy)
