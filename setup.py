0dd897f2e6aca10edb05d282d523f9ccbcdc49ac
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-08 15:21:03 +0100

    Changed pip strategy

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sibyl',
+    version='0.0.3',
+    author='Pedro Cavalheiro',
+    author_email='pedro.cavalheiro@isat.pt',
+    description='Sibyl static site generator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/isat-sibyl/sibyl',
+    project_urls = {
+        "Bug Tracker": "https://github.com/isat-sibyl/sibyl/issues"
+    },
+    license='All rights reserved',
+    packages=['sibyl'],
+    install_requires=[
+			'beautifulsoup4==4.12.2',
+			'PyYAML==6.0',
+			'soupsieve==2.4',
+			'watchdog==3.0.0',
+			'websockets==11.0.1'
+		],
+)
