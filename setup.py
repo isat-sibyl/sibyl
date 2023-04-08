5062da72b02b7d41ecaa7812833568d699d254d6
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-08 16:17:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='sibyl',
-    version='0.0.16',
+    version='0.0.17',
     author='Pedro Cavalheiro',
     author_email='pedro.cavalheiro@isat.pt',
     description='Sibyl static site generator',
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     license='All rights reserved',
     packages=['sibyl'],
-    package_data={'sibyl': ['components/*', 'layouts/*', 'locales/*', 'pages/*', 'static/*', 'settings.yaml', 'hot-reload.html', 'dev.bat', 'build.bat']},
+    package_data={'sibyl': ['components/*', 'layouts/*', 'locales/*', 'locales/.global.json', 'pages/*', 'static/*', 'settings.yaml', 'hot-reload.html', 'dev.bat', 'build.bat']},
     include_package_data=True,
     install_requires=[
 			'beautifulsoup4==4.12.2',
