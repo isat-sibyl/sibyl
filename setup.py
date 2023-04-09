703661e935aed82d8b14ceff97d2e5f9daf0fa6c
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-09 17:29:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     license='All rights reserved',
     packages=['sibyl'],
-    package_data={'sibyl': ['components/*', 'layouts/*', 'locales/*', 'locales/.global.json', 'pages/*', 'static/*', 'sibyl-static/*', 'settings.yaml', 'hot-reload.html', 'Pipfile', 'requirements.txt']},
+    package_data={'sibyl': ['components/*', 'layouts/*', 'locales/*', 'locales/.global.json', 'pages/*', 'static/*', 'sibyl-static/*', 'static/favicon/*', 'settings.yaml', 'hot-reload.html', 'Pipfile', 'requirements.txt']},
     include_package_data=True,
     install_requires=[
 			'beautifulsoup4>=4.12.2',
