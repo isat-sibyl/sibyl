8379c52a78af80518bc118c8b523ed2ca4ee9b0a
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-09 23:38:34 +0100

    Update sibyl.py

--- sibyl/sibyl.py
+++ sibyl/sibyl.py
@@ -11,16 +11,19 @@ def help():
 	print('  help - Show this help message')
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		if sys.argv[1] == 'dev':
-			os.system('python -m sibyl.dev')
-		elif sys.argv[1] == 'build':
-			os.system('python -m sibyl.build')
-		elif sys.argv[1] == 'init':
-			os.system('python -m sibyl.init')
-		elif sys.argv[1] == 'help':
-			help()
+	try:
+		if len(sys.argv) > 1:
+			if sys.argv[1] == 'dev':
+				os.system('python -m sibyl.dev')
+			elif sys.argv[1] == 'build':
+				os.system('python -m sibyl.build')
+			elif sys.argv[1] == 'init':
+				os.system('python -m sibyl.init')
+			elif sys.argv[1] == 'help':
+				help()
+			else:
+				print('Unknown command. Run "sibyl help" for more information.')
 		else:
-			print('Unknown command. Run "sibyl help" for more information.')
-	else:
-		os.system('Unknown command. Run "sibyl help" for more information.')
+			os.system('Unknown command. Run "sibyl help" for more information.')
+	except KeyboardInterrupt:
+		os._exit(0) 
