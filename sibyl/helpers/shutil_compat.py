3d4a4e722923ceda75f1764012c49132ef8be5c7
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-16 17:37:34 +0100

    Compat version for python 3.7

--- sibyl/helpers/shutil_compat.py
+++ sibyl/helpers/shutil_compat.py
@@ -1,12 +1,14 @@
-import os
 import shutil
+import sys
 
 
 def copytree(src, dst, symlinks=False, ignore=None, dirs_exist_ok=False):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+    # if python version is 3.8 or higher, use the built-in copytree
+    if sys.version_info >= (3, 8):
+        shutil.copytree(
+            src, dst, symlinks=symlinks, ignore=ignore, dirs_exist_ok=dirs_exist_ok
+        )
+    else:
+        from distutils import dir_util
+
+        dir_util.copy_tree(src, dst)
