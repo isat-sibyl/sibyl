fef9fc5a0e9bc4254767fc2f45128378dedcb231
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2023-04-16 14:55:12 +0100

    Update shutil_compat.py

--- sibyl/helpers/shutil_compat.py
+++ sibyl/helpers/shutil_compat.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 
-def copytree(src, dst, symlinks=False, ignore=None):
+def copytree(src, dst, symlinks=False, ignore=None, dirs_exist_ok=False):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
